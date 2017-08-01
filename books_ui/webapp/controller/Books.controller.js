e8f9e47dd117123aab5498b76df1d30d07d53c24
I059508 <ran.hassid@sap.com>
2017-08-01 06:15:31 +0000

    instantiate model via manifest.json

--- books_ui/webapp/controller/Books.controller.js
+++ books_ui/webapp/controller/Books.controller.js
@@ -5,15 +5,8 @@ sap.ui.define([
 
 	return Controller.extend("com.sap.sample.books_ui.controller.Books", {
 		onInit: function() {
-			
-			// $.get("/odata/JDBCDemo.db._.store/");
-			
-			var oModel = new sap.ui.model.odata.v4.ODataModel({
-				serviceUrl: "/odata/JDBCDemo.db._.store/",
-				synchronizationMode: "None"
-			});
-			
-			this.getView().setModel(oModel);
+
+
 		}
 	});
 });
